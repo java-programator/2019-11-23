d82c79584a7f6595fb2362eace58a0c6f53b68f3
DawidWatroba <torba135711>
2019-11-23 12:19:09 +0100

    thread example2 with addition decrementation

--- src/pl/altkom/threads/Example02.java
+++ src/pl/altkom/threads/Example02.java
@@ -1,21 +1,34 @@
 package pl.altkom.threads;
 
-class Task02 implements Runnable {
+class TaskIncrement implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 1000000; i++) {
             Example02.counter++;
         }
-        System.out.println("Skończyłem");
+        System.out.println("Skończyłem inkrementować "+Example02.counter);
+    }
+}
+class TaskDecrement implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000000; i++) {
+            Example02.counter--;
+        }
+        System.out.println("Skończyłem dekrementować "+Example02.counter);
     }
 }
 
 public class Example02 {
     static int counter = 0;
     public static void main(String[] args) throws InterruptedException {
-            Thread thread = new Thread(new Task02());
-            thread.start();
-            thread.join();
+            Thread t1 = new Thread(new TaskIncrement());
+            Thread t2 = new Thread(new TaskDecrement());
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
             System.out.println(counter);
         }
 
