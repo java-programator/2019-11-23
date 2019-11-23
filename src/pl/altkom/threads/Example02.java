a371757bc12a5cb63f7696205cbc4216adf6185a
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-11-23 13:10:02 +0100

    przykład 3 z wątkami

--- src/pl/altkom/threads/Example02.java
+++ src/pl/altkom/threads/Example02.java
@@ -5,20 +5,39 @@ class IncrementTask implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 1000000; i++) {
-            Example02.counter++;
+            synchronized (Example02.semaphore) {
+                Example02.counter++;
+            }
         }
-        System.out.println("Skończyłem " + Example02.counter);
+        System.out.println("Skończyłem dodawać " + Example02.counter);
+    }
+}
+
+class DecrementTask implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000000; i++) {
+            synchronized (Example02.semaphore) {
+                Example02.counter--;
+            }
+        }
+        System.out.println("Skończyłem odejmować " + Example02.counter);
     }
 }
 
 public class Example02 {
 
     static int counter = 0;
+    static Object semaphore = new Object();
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t = new Thread(new IncrementTask());
-        t.start();
-        t.join();
+        Thread t1 = new Thread(new IncrementTask());
+        Thread t2 = new Thread(new DecrementTask());
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
         System.out.println(counter);
     }
 }
